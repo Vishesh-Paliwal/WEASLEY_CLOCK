ac98abf249b4e44e1d8721974e0a4161fe4875fe
Vishesh-Paliwal <142072830+Vishesh-Paliwal@users.noreply.github.com>
2024-02-04 03:32:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 var initiate_clock = function() {
-    var second_hand = document.getElementById("second");
-    var minute_hand = document.getElementById("minute");
-    var hour_hand = document.getElementById("hour");
+    var second_hand = document.getElementById("second_hand");
+    var minute_hand = document.getElementById("minute_hand");
+    var hour_hand = document.getElementById("hour_hand");
 
     var updateClockHands = function() {
         var now = new Date();
@@ -10,9 +10,9 @@ var initiate_clock = function() {
         var minutes = now.getMinutes();
         var hours = now.getHours() % 12;
 
-        var secondsRotation = (seconds * 6 - 90); 
-        var minutesRotation = (minutes * 6 - 90);
-        var hoursRotation = (hours * 30 - 90) + Math.round(minutesRotation/12); 
+        var secondsRotation = (seconds * 6); 
+        var minutesRotation = (minutes * 6);
+        var hoursRotation = (hours * 30) + Math.round(minutesRotation/12); 
 
         second_hand.style.transform = "rotate(" + secondsRotation + "deg)";
         minute_hand.style.transform = "rotate(" + minutesRotation + "deg)";
@@ -33,6 +33,7 @@ var on = true;
 button.addEventListener("click",function(){
     const container = document.querySelector("#container")
     if(on==true){
+        console.log("clicked");
     container.classList.add("levitate")
     theme.pause()
     spell.play();
